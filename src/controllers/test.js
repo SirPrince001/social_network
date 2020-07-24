312f7ef2ba08d9e1c9ae061e712431210d65eddd
Prince Bassey <basseyprince75@gmail.com>
2020-07-24 21:38:52 +0100

    "updated test token" wq

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -4,8 +4,8 @@ const { Response, ResponseError } = require("../utils/response");
 
 exports.tokenVerify = (request) => {
   const token = request.body.jwt;
-  console.log(token)
-  console.log(process.env.SECRETKEY)
+  // console.log(token)
+  // console.log(process.env.SECRETKEY)
   let payload = jwt.verify(token, process.env.SECRETKEY);
     console.log(x)
   return new Response(200, payload);
